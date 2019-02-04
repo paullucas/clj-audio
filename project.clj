fe3f57be80741375d4660f97e79e79f43b5550e3
Paul <paulll.lucas@gmail.com>
2019-02-03 21:25:45 -0800

    Upgrade to Clojure 1.10

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject clj-audio/clj-audio "0.3.0-SNAPSHOT"
   :description "Idiomatic Clojure wrapper for the Java Sound API."
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]])
